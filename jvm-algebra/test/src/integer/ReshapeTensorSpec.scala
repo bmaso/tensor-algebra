82c207980ac12a0eabc1898a14dbd17b886b5083
Brian Maso <brian@blumenfeld-maso.com>
2020-05-07 09:59:57 -0700

    JVM integer unit tests for PivotTensor, ReshapeTensor, ReverseTensor and SliceTensor parameterized for Byte, Short, Int, Long, Float and Double

--- jvm-algebra/test/src/integer/ReshapeTensorSpec.scala
+++ jvm-algebra/test/src/integer/ReshapeTensorSpec.scala
@@ -1,29 +1,30 @@
 package bmaso.tensoralg.jvm.integer
 
 import scala.language.postfixOps
+import scala.reflect.ClassTag
 
 import bmaso.tensoralg.abstractions._
 import org.scalatest.{FlatSpec, Matchers}, Matchers._
 
-class ReshapeTensorSpec extends FlatSpec {
+abstract class ReshapeTensorSpecBase[T: Numeric: ClassTag: ConvertsInt] extends TensorFlatSpecBase[T] {
   "A 3x4x5 ReshapeTensor made from a 10x2x3 tensor" should "have the expected magnitude, order, elementSize and element values" in {
-    val arrayTensor = ArrayTensor[Int]((0 to 59) toArray, Array(10, 2, 3), 0)
-    val reshaped = ReshapeTensor(arrayTensor, Array(3, 4, 5))
+    val arrayTensor = ArrayTensor[T]((0 to 59).toArray.asTArray, Array(10, 2, 3), 0)
+    val reshaped = ReshapeTensor[T](arrayTensor, Array(3, 4, 5))
 
     reshaped.magnitude should be (Array(3, 4, 5))
     reshaped.order should be (3)
     reshaped.elementSize should be (60)
 
-    reshaped.valueAt(Array(0, 0, 0)) should be (0)
-    reshaped.valueAt(Array(2, 2, 1)) should be (20)
-    reshaped.valueAt(Array(0, 1, 4)) should be (51)
-    reshaped.valueAt(Array(1, 3, 2)) should be (34)
-    reshaped.valueAt(Array(2, 3, 4)) should be (59)
+    reshaped.valueAt(Array(0, 0, 0)).asI should be (0)
+    reshaped.valueAt(Array(2, 2, 1)).asI should be (20)
+    reshaped.valueAt(Array(0, 1, 4)).asI should be (51)
+    reshaped.valueAt(Array(1, 3, 2)).asI should be (34)
+    reshaped.valueAt(Array(2, 3, 4)).asI should be (59)
   }
 
   "Providing additional dimension index 0 values when getting values from a ReshapedTensor" should "yield the same as not providing those values" in {
-    val arrayTensor = ArrayTensor[Int]((0 to 59) toArray, Array(10, 2, 3), 0)
-    val reshaped = ReshapeTensor(arrayTensor, Array(3, 4, 5))
+    val arrayTensor = ArrayTensor[T]((0 to 59).toArray.asTArray, Array(10, 2, 3), 0)
+    val reshaped = ReshapeTensor[T](arrayTensor, Array(3, 4, 5))
 
     reshaped.valueAt(Array(0, 0, 0)) should be (reshaped.valueAt(Array(0, 0, 0, 0)))
     reshaped.valueAt(Array(2, 2, 1)) should be (reshaped.valueAt(Array(2, 2, 1, 0, 0)))
@@ -32,3 +33,10 @@ class ReshapeTensorSpec extends FlatSpec {
     reshaped.valueAt(Array(2, 3, 4)) should be (reshaped.valueAt(Array(2, 3, 4, 0, 0, 0, 0)))
   }
 }
+
+class ByteReshapeTensorSpecBase extends ReshapeTensorSpecBase[Byte]
+class ShortReshapeTensorSpecBase extends ReshapeTensorSpecBase[Short]
+class IntReshapeTensorSpecBase extends ReshapeTensorSpecBase[Int]
+class LongReshapeTensorSpecBase extends ReshapeTensorSpecBase[Long]
+class FloatReshapeTensorSpecBase extends ReshapeTensorSpecBase[Float]
+class DoubleReshapeTensorSpecBase extends ReshapeTensorSpecBase[Double]
--- jvm-algebra/test/src/integer/ReverseTensorSpec.scala
+++ jvm-algebra/test/src/integer/ReverseTensorSpec.scala
@@ -1,75 +1,83 @@
 package bmaso.tensoralg.jvm.integer
 
 import scala.language.postfixOps
+import scala.reflect.ClassTag
 
 import bmaso.tensoralg.abstractions._
 import org.scalatest.{FlatSpec, Matchers}, Matchers._
 
-class ReverseTensorSpec extends FlatSpec {
+abstract class ReverseTensorSpecBase[T: Numeric: ClassTag: ConvertsInt] extends TensorFlatSpecBase[T] {
   "A reverse tensor constructed from a 1D source tensor" should "have the expected magntude, order, elementSize and element values" in {
-    val sourceTensor = ArrayTensor[Int]((0 to 5) toArray, Array(6), 0)
-    val reversed = ReverseTensor(sourceTensor, _X)
+    val sourceTensor = ArrayTensor[T]((0 to 5).toArray.asTArray, Array(6), 0)
+    val reversed = ReverseTensor[T](sourceTensor, _X)
 
     reversed.order should be (1)
     reversed.magnitude(_X) should be (6)
     reversed.elementSize should be (6)
 
-    reversed.valueAt(Array(0)) should be (5)
-    reversed.valueAt(Array(1)) should be (4)
-    reversed.valueAt(Array(2)) should be (3)
-    reversed.valueAt(Array(3)) should be (2)
-    reversed.valueAt(Array(4)) should be (1)
-    reversed.valueAt(Array(5)) should be (0)
+    reversed.valueAt(Array(0)).asI should be (5)
+    reversed.valueAt(Array(1)).asI should be (4)
+    reversed.valueAt(Array(2)).asI should be (3)
+    reversed.valueAt(Array(3)).asI should be (2)
+    reversed.valueAt(Array(4)).asI should be (1)
+    reversed.valueAt(Array(5)).asI should be (0)
   }
 
   "Reversing a tensor in a unitary dimension" should "have no effect" in {
-    val sourceTensor = ArrayTensor[Int]((0 to 5) toArray, Array(6, 1), 0)
-    val reversed = ReverseTensor(sourceTensor, _Y)
+    val sourceTensor = ArrayTensor[T]((0 to 5).toArray.asTArray, Array(6, 1), 0)
+    val reversed = ReverseTensor[T](sourceTensor, _Y)
 
     reversed.order should be (2)
     reversed.magnitude(_X) should be (6)
     reversed.elementSize should be (6)
 
-    reversed.valueAt(Array(0, 0)) should be (0)
-    reversed.valueAt(Array(1, 0)) should be (1)
-    reversed.valueAt(Array(2, 0)) should be (2)
-    reversed.valueAt(Array(3, 0)) should be (3)
-    reversed.valueAt(Array(4, 0)) should be (4)
-    reversed.valueAt(Array(5, 0)) should be (5)
+    reversed.valueAt(Array(0, 0)).asI should be (0)
+    reversed.valueAt(Array(1, 0)).asI should be (1)
+    reversed.valueAt(Array(2, 0)).asI should be (2)
+    reversed.valueAt(Array(3, 0)).asI should be (3)
+    reversed.valueAt(Array(4, 0)).asI should be (4)
+    reversed.valueAt(Array(5, 0)).asI should be (5)
   }
 
   "Reversing a tensor in a higher dimension than the order in  a 3x3 tensor" should "have no effect" in {
-    val sourceTensor = ArrayTensor[Int]((0 to 8) toArray, Array(3, 3), 0)
-    val reversed = ReverseTensor(sourceTensor, _T)
+    val sourceTensor = ArrayTensor[T]((0 to 8).toArray.asTArray, Array(3, 3), 0)
+    val reversed = ReverseTensor[T](sourceTensor, _T)
 
     reversed.order should be (2)
     reversed.magnitude should be (Array(3, 3))
     reversed.elementSize should be (9)
 
     for(i <- 0 to 8) {
-      reversed.valueAt1D(i) should be (i)
+      reversed.valueAt1D(i).asI should be (i)
     }
   }
 
   "A tensor constructed by reversing an order-3 tensor in the _Y dimension" should "have the expected magnitude, order, elementSize and elementValues" in {
-    val sourceTensor = ArrayTensor[Int]((0 to 11) toArray, Array(2, 3, 2), 0)
-    val reversed = ReverseTensor(sourceTensor, _Y)
+    val sourceTensor = ArrayTensor[T]((0 to 11).toArray.asTArray, Array(2, 3, 2), 0)
+    val reversed = ReverseTensor[T](sourceTensor, _Y)
 
     reversed.order should be (3)
     reversed.magnitude should be (Array(2, 3, 2))
     reversed.elementSize should be (12)
 
-    reversed.valueAt(Array(0, 0, 0)) should be (4)
-    reversed.valueAt(Array(1, 0, 0)) should be (5)
-    reversed.valueAt(Array(0, 1, 0)) should be (2)
-    reversed.valueAt(Array(1, 1, 0)) should be (3)
-    reversed.valueAt(Array(0, 2, 0)) should be (0)
-    reversed.valueAt(Array(1, 2, 0)) should be (1)
-    reversed.valueAt(Array(0, 0, 1)) should be (10)
-    reversed.valueAt(Array(1, 0, 1)) should be (11)
-    reversed.valueAt(Array(0, 1, 1)) should be (8)
-    reversed.valueAt(Array(1, 1, 1)) should be (9)
-    reversed.valueAt(Array(0, 2, 1)) should be (6)
-    reversed.valueAt(Array(1, 2, 1)) should be (7)
+    reversed.valueAt(Array(0, 0, 0)).asI should be (4)
+    reversed.valueAt(Array(1, 0, 0)).asI should be (5)
+    reversed.valueAt(Array(0, 1, 0)).asI should be (2)
+    reversed.valueAt(Array(1, 1, 0)).asI should be (3)
+    reversed.valueAt(Array(0, 2, 0)).asI should be (0)
+    reversed.valueAt(Array(1, 2, 0)).asI should be (1)
+    reversed.valueAt(Array(0, 0, 1)).asI should be (10)
+    reversed.valueAt(Array(1, 0, 1)).asI should be (11)
+    reversed.valueAt(Array(0, 1, 1)).asI should be (8)
+    reversed.valueAt(Array(1, 1, 1)).asI should be (9)
+    reversed.valueAt(Array(0, 2, 1)).asI should be (6)
+    reversed.valueAt(Array(1, 2, 1)).asI should be (7)
   }
 }
+
+class ByteReverseTensorSpec extends ReverseTensorSpecBase[Byte]
+class ShortReverseTensorSpec extends ReverseTensorSpecBase[Short]
+class IntReverseTensorSpec extends ReverseTensorSpecBase[Int]
+class LongReverseTensorSpec extends ReverseTensorSpecBase[Long]
+class FloatReverseTensorSpec extends ReverseTensorSpecBase[Float]
+class DoubleReverseTensorSpec extends ReverseTensorSpecBase[Double]
