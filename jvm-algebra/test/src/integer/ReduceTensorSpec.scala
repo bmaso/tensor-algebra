a7f0027c64c18b16a34cb85b85ee8b3d69f40d24
Brian Maso <brian@blumenfeld-maso.com>
2020-05-08 09:19:33 -0700

    All JVM integer tensor unit tests have been converted to parameterized Byte, Short, Int, Long, FLoat and Double forms

--- jvm-algebra/test/src/integer/ReduceTensorSpec.scala
+++ jvm-algebra/test/src/integer/ReduceTensorSpec.scala
@@ -1,84 +1,77 @@
 package bmaso.tensoralg.jvm.integer
 
 import scala.language.postfixOps
+import scala.reflect.ClassTag
 
 import bmaso.tensoralg.abstractions._
 import org.scalatest.{FlatSpec, Matchers}, Matchers._
 
-class ReduceTensorSpec extends FlatSpec {
+class ReduceTensorSpec[T: Numeric: ClassTag: ConvertsInt] extends TensorFlatSpecBase[T] {
 
-  def sum(t: JVMTensor[Int]): Int = (0 to (t.elementSize.toInt - 1)).map(t.valueAt1D(_)).sum
+  def sum(t: JVMTensor[T]): T = (0 to (t.elementSize.toInt - 1)).map(t.valueAt1D(_)).sum
 
   "a 3x3 tensor reduced in 2 orders" should "yield a scalar tensor" in {
-    import IntTensorAlgebra._
-
-    val arrayTensor = ArrayTensor[Int]((0 to 8).map(_ => 1) toArray, Array(3, 3), 0)
-    val reduce = ReduceTensor[Int](arrayTensor, 2, sum)
+    val arrayTensor = ArrayTensor[T]((0 to 8).map(_ => 1).toArray.asTArray, Array(3, 3), 0)
+    val reduce = ReduceTensor[T](arrayTensor, 2, sum)
 
     reduce.magnitude should be (Array(1))
     reduce.order should be (1)
     reduce.elementSize should be (1)
 
-    reduce.valueAt(Array(0)) should be (9)
+    reduce.valueAt(Array(0)).asI should be (9)
   }
 
   "a 3x3 tensor reduced in 1 order" should "yield a 1-D tensor with magnitude, order, elementSize and element values as expected" in {
-    import IntTensorAlgebra._
-
-    val arrayTensor = ArrayTensor[Int]((0 to 8) toArray, Array(3, 3), 0)
-    val reduce = ReduceTensor[Int](arrayTensor, 1, sum)
+    val arrayTensor = ArrayTensor[T]((0 to 8).toArray.asTArray, Array(3, 3), 0)
+    val reduce = ReduceTensor[T](arrayTensor, 1, sum)
 
     reduce.magnitude should be (Array(3))
     reduce.order should be (1)
     reduce.elementSize should be (3)
 
-    reduce.valueAt(Array(0)) should be (3)
-    reduce.valueAt(Array(1)) should be (12)
-    reduce.valueAt(Array(2)) should be (21)
+    reduce.valueAt(Array(0)).asI should be (3)
+    reduce.valueAt(Array(1)).asI should be (12)
+    reduce.valueAt(Array(2)).asI should be (21)
   }
 
   "a 3x3 tensor reduced in 0 orders" should "yield a 2-D tensor with magnitude, order, elementSize and element values as expected" in {
-    import IntTensorAlgebra._
-
-    val arrayTensor = ArrayTensor[Int]((0 to 8) toArray, Array(3, 3), 0)
-    val reduce = ReduceTensor[Int](arrayTensor, 0, (t => t.valueAt(Array(0)) * 2))
+    val arrayTensor = ArrayTensor[T]((0 to 8).toArray.asTArray, Array(3, 3), 0)
+    val reduce = ReduceTensor[T](arrayTensor, 0, (t => implicitly[Numeric[T]].times(t.valueAt(Array(0)), 2.asT)))
 
     reduce.magnitude should be (Array(3, 3))
     reduce.order should be (2)
     reduce.elementSize should be (9)
 
-    reduce.valueAt(Array(0, 0)) should be (0)
-    reduce.valueAt(Array(1, 0)) should be (2)
-    reduce.valueAt(Array(2, 0)) should be (4)
-    reduce.valueAt(Array(0, 1)) should be (6)
-    reduce.valueAt(Array(1, 1)) should be (8)
-    reduce.valueAt(Array(2, 1)) should be (10)
-    reduce.valueAt(Array(0, 2)) should be (12)
-    reduce.valueAt(Array(1, 2)) should be (14)
-    reduce.valueAt(Array(2, 2)) should be (16)
+    reduce.valueAt(Array(0, 0)).asI should be (0)
+    reduce.valueAt(Array(1, 0)).asI should be (2)
+    reduce.valueAt(Array(2, 0)).asI should be (4)
+    reduce.valueAt(Array(0, 1)).asI should be (6)
+    reduce.valueAt(Array(1, 1)).asI should be (8)
+    reduce.valueAt(Array(2, 1)).asI should be (10)
+    reduce.valueAt(Array(0, 2)).asI should be (12)
+    reduce.valueAt(Array(1, 2)).asI should be (14)
+    reduce.valueAt(Array(2, 2)).asI should be (16)
   }
 
-  "a 6x5x4x3 tensor reduced in 2 orders" should "yield a 4x3 tensor with magnitude, order, elementSize and element values as expected" in {
-    import IntTensorAlgebra._
-
-    val arrayTensor = ArrayTensor[Int]((0 to 359).map(_ => 1) toArray, Array(6, 5, 4, 3), 0)
-    val reduce = ReduceTensor[Int](arrayTensor, 2, sum)
+  "a 4x3x4x3 tensor reduced in 2 orders" should "yield a 4x3 tensor with magnitude, order, elementSize and element values as expected" in {
+    val arrayTensor = ArrayTensor[T]((0 to 127).map(_ => 1).toArray.asTArray, Array(4, 3, 4, 3), 0)
+    val reduce = ReduceTensor[T](arrayTensor, 2, sum)
 
     reduce.magnitude should be (Array(4, 3))
     reduce.order should be (2)
     reduce.elementSize should be (12)
 
-    reduce.valueAt(Array(0, 0)) should be (30)
-    reduce.valueAt(Array(1, 0)) should be (30)
-    reduce.valueAt(Array(2, 0)) should be (30)
-    reduce.valueAt(Array(3, 0)) should be (30)
-    reduce.valueAt(Array(0, 1)) should be (30)
-    reduce.valueAt(Array(1, 1)) should be (30)
-    reduce.valueAt(Array(2, 1)) should be (30)
-    reduce.valueAt(Array(3, 1)) should be (30)
-    reduce.valueAt(Array(0, 2)) should be (30)
-    reduce.valueAt(Array(1, 2)) should be (30)
-    reduce.valueAt(Array(2, 2)) should be (30)
-    reduce.valueAt(Array(3, 2)) should be (30)
+    reduce.valueAt(Array(0, 0)) should be (12)
+    reduce.valueAt(Array(1, 0)) should be (12)
+    reduce.valueAt(Array(2, 0)) should be (12)
+    reduce.valueAt(Array(3, 0)) should be (12)
+    reduce.valueAt(Array(0, 1)) should be (12)
+    reduce.valueAt(Array(1, 1)) should be (12)
+    reduce.valueAt(Array(2, 1)) should be (12)
+    reduce.valueAt(Array(3, 1)) should be (12)
+    reduce.valueAt(Array(0, 2)) should be (12)
+    reduce.valueAt(Array(1, 2)) should be (12)
+    reduce.valueAt(Array(2, 2)) should be (12)
+    reduce.valueAt(Array(3, 2)) should be (12)
   }
 }
--- jvm-algebra/test/src/integer/StackTensorSpec.scala
+++ jvm-algebra/test/src/integer/StackTensorSpec.scala
@@ -1,54 +1,60 @@
 package bmaso.tensoralg.jvm.integer
 
 import scala.language.postfixOps
+import scala.reflect.ClassTag
 
 import cats.implicits._
 import org.scalatest.{FlatSpec, Matchers}, Matchers._
 
 import bmaso.tensoralg.abstractions._
 
-class StackTensorSpec extends FlatSpec {
+class StackTensorSpecBase[T: Numeric: ClassTag: ConvertsInt] extends TensorFlatSpecBase[T] {
   "A stack of 2 3x2 tensors in the _Z dimension" should "have the expected magnitude, order, elementSize and elemetn values" in {
-    import IntTensorAlgebra._
-
-    val t1 = ArrayTensor[Int]((0 to 5) toArray, Array(3, 2), 0)
-    val t2 = ArrayTensor[Int]((6 to 11) toArray, Array(3, 2), 0)
-    val stack = StackTensor[Int](Array(t1, t2), _Z)
+    val t1 = ArrayTensor[T]((0 to 5).toArray.asTArray, Array(3, 2), 0)
+    val t2 = ArrayTensor[T]((6 to 11).toArray.asTArray, Array(3, 2), 0)
+    val stack = StackTensor[T](Array(t1, t2), _Z)
 
     stack.magnitude should be (Array(3, 2, 2))
     stack.order should be (3)
     stack.elementSize should be (12)
 
     for(ii <- 0 to 11) {
-      stack.valueAt1D(ii) should be (ii)
+      stack.valueAt1D(ii).asI should be (ii)
     }
 
     //...a couple index-based element size checks to make sure allis well...
-    stack.valueAt(Array(2, 1, 1)) should be (11)
-    stack.valueAt(Array(0, 1, 0)) should be (3)
-    stack.valueAt(Array(1, 0, 1)) should be (7)
+    stack.valueAt(Array(2, 1, 1)).asI should be (11)
+    stack.valueAt(Array(0, 1, 0)).asI should be (3)
+    stack.valueAt(Array(1, 0, 1)).asI should be (7)
   }
 
   "A stack of 2 1x2x3 tensors in the _X direction" should "have the expected magnitude, order, elementSize and elemetn values" in {
-    val t1 = ArrayTensor[Int]((0 to 5) toArray, Array(1, 2, 3), 0)
-    val t2 = ArrayTensor[Int]((6 to 11) toArray, Array(1, 2, 3), 0)
-    val stack = StackTensor[Int](Array(t1, t2), _X)
+    val t1 = ArrayTensor[T]((0 to 5).toArray.asTArray, Array(1, 2, 3), 0)
+    val t2 = ArrayTensor[T]((6 to 11).toArray.asTArray, Array(1, 2, 3), 0)
+    val stack = StackTensor[T](Array(t1, t2), _X)
 
     stack.magnitude should be (Array(2, 2, 3))
     stack.order should be (3)
     stack.elementSize should be (12)
 
-    stack.valueAt(Array(0, 0, 0)) should be (0)
-    stack.valueAt(Array(1, 0, 0)) should be (6)
-    stack.valueAt(Array(0, 1, 0)) should be (1)
-    stack.valueAt(Array(1, 1, 0)) should be (7)
-    stack.valueAt(Array(0, 0, 1)) should be (2)
-    stack.valueAt(Array(1, 0, 1)) should be (8)
-    stack.valueAt(Array(0, 1, 1)) should be (3)
-    stack.valueAt(Array(1, 1, 1)) should be (9)
-    stack.valueAt(Array(0, 0, 2)) should be (4)
-    stack.valueAt(Array(1, 0, 2)) should be (10)
-    stack.valueAt(Array(0, 1, 2)) should be (5)
-    stack.valueAt(Array(1, 1, 2)) should be (11)
+    stack.valueAt(Array(0, 0, 0)).asI should be (0)
+    stack.valueAt(Array(1, 0, 0)).asI should be (6)
+    stack.valueAt(Array(0, 1, 0)).asI should be (1)
+    stack.valueAt(Array(1, 1, 0)).asI should be (7)
+    stack.valueAt(Array(0, 0, 1)).asI should be (2)
+    stack.valueAt(Array(1, 0, 1)).asI should be (8)
+    stack.valueAt(Array(0, 1, 1)).asI should be (3)
+    stack.valueAt(Array(1, 1, 1)).asI should be (9)
+    stack.valueAt(Array(0, 0, 2)).asI should be (4)
+    stack.valueAt(Array(1, 0, 2)).asI should be (10)
+    stack.valueAt(Array(0, 1, 2)).asI should be (5)
+    stack.valueAt(Array(1, 1, 2)).asI should be (11)
   }
 }
+
+class ByteStackTensorSpec extends StackTensorSpecBase[Byte]
+class ShortStackTensorSpec extends StackTensorSpecBase[Short]
+class IntStackTensorSpec extends StackTensorSpecBase[Int]
+class LongStackTensorSpec extends StackTensorSpecBase[Long]
+class FloatStackTensorSpec extends StackTensorSpecBase[Float]
+class DoubleStackTensorSpec extends StackTensorSpecBase[Double]
